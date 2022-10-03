ebcafd7aabfb16d01f334ba845409d8946911cba
Abdurehman Saeed <asaeed526@gmail.com>
2022-10-03 15:38:59 +0300

    display text edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-
-
 import './App.css';
 import FiveBandHome from './components/pages/fiveBand/FiveBandHome';
-import FiveBandResistor from './components/pages/fiveBand/FiveBandResistor';
 import FourBandHome from './components/pages/fourBand/FourBandHome';
 import SixBandHome from './components/pages/sixBand/SixBandHome';
 import FourBand from './images/fourBandManual.jpg'
-import FiveBand from './images/fiveBandManual.jpg'
-import sixBand from './images/sixBandManual.jpg'
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import { NavBar } from './components/NavBar';
 import Modal from './components/Modal';
-import { useEffect, useState } from 'react';
-import LoadingPage from './components/LoadingPage';
-
+import { useState } from 'react';
 
 
 function App() {
